fb50516225cdace7d57ecbdca3bc3e5c73382d5a
Zain Ahmed <zainahmed1559@gmail.com>
2017-06-30 14:23:31 -0400

    more buttonz

--- script.js
+++ script.js
@@ -15,8 +15,16 @@ function joinRoom() {
     alert('shit');
     var div = document.createElement("div");
     div.id = "roomBox"
+
     var joinRoomButton = document.createElement("button")
-    joinRoomButton.className = "buttons1"
+    joinRoomButton.innerText = "Join a Room";
+    joinRoomButton.className = "buttons1";
+
+    var createRoomButton = document.createElement("button");
+    createRoomButton.innerText = "Create a Room";
+    createRoomButton.className = "buttons1";
+
+    div.appendChild(createRoomButton);
     div.appendChild(joinRoomButton);
     document.getElementsByTagName('body')[0].appendChild(div);
 }
