ec5f3ae4c38dd5bc7fd06a780bc781e44bcf4667
Zain Ahmed <zainahmed1559@gmail.com>
2017-06-30 15:39:50 -0400

    added functions that are different for each function

--- script.js
+++ script.js
@@ -21,11 +21,13 @@ function joinRoom() {
     joinRoomButton.innerText = "Join a Room";
     joinRoomButton.onclick = clearAll;
     joinRoomButton.className = "buttons1";
+    joinRoomButton.id = "joinroombutton";
 
     var createRoomButton = document.createElement("button");
     createRoomButton.innerText = "Create a Room";
     createRoomButton.className = "buttons1";
     createRoomButton.onclick = clearAll;
+    createRoomButton.id = "createroombutton";
 
     var welcomeBanner = document.createElement("h1");
     welcomeBanner.innerText = "Welcome";
@@ -40,10 +42,26 @@ function joinRoom() {
 
 
 function clearAll() {
+    var sender = this.id;
     body.innerHTML = "";
-
     var logo = document.createElement("h1");
     logo.innerText = "DJSync";
     body.appendChild(logo);
+
+
+
+    switch (sender) {
+        case ("createroombutton"):
+            console.log(sender + " was pressed");
+            createRoom();
+            break;
+        case ("joinroombutton"):
+            console.log(sender + " was pressed");
+            joinRoom();
+            break;
+        default:
+            //console.log(sender);
+            break;
+    }
 
 }
