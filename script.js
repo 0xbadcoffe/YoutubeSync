2df055ebe243fdc7c3ce3a2e79ef941ded08d16f
Zain Ahmed <zainahmed1559@gmail.com>
2017-06-30 13:34:03 -0400

    barebones

---
+++ script.js
@@ -0,0 +1,13 @@
+//firebase initialization
+
+var config = {
+    apiKey: "AIzaSyAH7CQ-JuB5KFJqFGrQXIF_NxMp-glz6PY",
+    authDomain: "music-sync-8212d.firebaseapp.com",
+    databaseURL: "https://music-sync-8212d.firebaseio.com",
+    projectId: "music-sync-8212d",
+    storageBucket: "music-sync-8212d.appspot.com",
+    messagingSenderId: "317967865570"
+};
+firebase.initializeApp(config);
+
+alert("fuck you");
