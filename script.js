690245dba9c05dcaff7c1da56d19b7c1536b1842
Zain Ahmed <zainahmed1559@gmail.com>
2017-07-05 14:48:14 -0400

    Sean rewrites the app...

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-//firebase initialization
-var config = {
-    apiKey: "AIzaSyAH7CQ-JuB5KFJqFGrQXIF_NxMp-glz6PY",
-    authDomain: "music-sync-8212d.firebaseapp.com",
-    databaseURL: "https://music-sync-8212d.firebaseio.com",
-    projectId: "music-sync-8212d",
-    storageBucket: "music-sync-8212d.appspot.com",
-    messagingSenderId: "317967865570"
-};
-firebase.initializeApp(config);
+function firebaseInit() {
+    //firebase initialization
+    var config = {
+        apiKey: "AIzaSyAH7CQ-JuB5KFJqFGrQXIF_NxMp-glz6PY",
+        authDomain: "music-sync-8212d.firebaseapp.com",
+        databaseURL: "https://music-sync-8212d.firebaseio.com",
+        projectId: "music-sync-8212d",
+        storageBucket: "music-sync-8212d.appspot.com",
+        messagingSenderId: "317967865570"
+    };
+    firebase.initializeApp(config);
+}
+
+var appHistory = [];
+
 
 //global vars
 var body = document.getElementsByTagName('body')[0];
 
 //Main homepage
+
 function welcomeButtonsPressed() {
     var div = document.createElement("div");
     div.id = "roomBox"
@@ -52,11 +58,11 @@ function clearAll() {
 
     switch (sender) {
         case ("createroombutton"):
-            console.log(sender + " was pressed");
+            //console.log(sender + " was pressed");
             createRoom();
             break;
         case ("joinroombutton"):
-            console.log(sender + " was pressed");
+            // console.log(sender + " was pressed");
             joinRoom();
             break;
         default:
@@ -67,10 +73,99 @@ function clearAll() {
 }
 
 function createRoom() {
-    console.log("nothing");
+    var div = document.createElement("div");
+    div.id = "roomBox"
+
+    var createTitle = document.createElement("h1");
+    createTitle.className = "createTitle";
+    createTitle.innerText = "Enter a Room Name"
+
+    var input = document.createElement("input");
+    input.placeholder = "case sensitive!";
+    input.className = "inputs1";
+
+    var createButton = document.createElement("button");
+    createButton.className = "buttons1";
+    createButton.innerText = "Launch";
+    createButton.style.marginTop = "20px";
+    createButton.onclick = "";
+
+
+    div.appendChild(createTitle);
+    div.appendChild(input);
+    div.appendChild(createButton);
+    div.style.height = "220px"
+
+    document.getElementsByTagName('body')[0].appendChild(div);
 }
 
 function joinRoom() {
-    console.log("nothing");
+    var div = document.createElement("div");
+    div.id = "roomBox"
 
 }
+
+// Loads a local HTML file and injects the HTML into the container element
+function appendTemplate(templateName) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `${templateName}.html`, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState !== 4) {
+            console.log('error', this.error);
+            return;
+        }
+        appendHistory(templateName);
+        document.getElementById('container').innerHTML = this.responseText;
+        bootstrapScripts(templateName);
+    };
+    xhr.send();
+}
+
+function appendHistory(templateName) {
+    if (appHistory.length < 1) {
+        appHistory.push(templateName);
+    } else {
+        const lastHistory = appHistory[appHistory.length - 1];
+        if (lastHistory !== templateName) {
+            appHistory.push(templateName);
+        }
+
+    }
+}
+
+function goBack() {
+    if (appHistory.length > 1) {
+        const templateName = appHistory[appHistory.length - 2];
+        appendTemplate(templateName);
+    }
+}
+
+function bootstrapScripts(templateName) {
+    switch (templateName) {
+        case 'test':
+            onSomeBtnClicked();
+            break;
+        case 'test1':
+            $('#testBtn').on('click', () => {
+                goBack();
+            });
+            break;
+    }
+}
+
+// Function called when the app initializes 
+function bootstrap() {
+    console.log('YoutubeSync Bootstrapping...');
+    firebaseInit();
+    appendTemplate('test');
+}
+
+function onSomeBtnClicked() {
+    $('#someBtn').on('click', () => {
+        appendTemplate('test1');
+    });
+}
+
+$(function() {
+    bootstrap();
+});
